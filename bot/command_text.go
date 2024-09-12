d5d45fc4fec426eda9bfb02e3c4c0d9244ea2f99
Pragmatic <cryptopragmatic@protonmail.com>
2024-09-13 00:54:34 +0200

    bot group work

--- bot/command_text.go
+++ bot/command_text.go
@@ -14,15 +14,20 @@ func textCommand(c telebot.Context) error {
 	var msg string
 
 	if m.Private() {
-		if m.Sender.ID != conf.OwnerId {
-			rec = &telebot.User{ID: conf.OwnerId}
-			msg = fmt.Sprintf("<b><u>%s:</u></b>\n%s",
-				m.Sender.FirstName,
-				m.Text)
-		} else if m.IsReply() {
-			msg = m.Text
-			rec = &telebot.User{ID: msgs[m.ReplyTo.ID]}
-		}
+		// if m.Sender.ID != conf.OwnerId {
+		// 	// rec = &telebot.User{ID: conf.OwnerId}
+		// 	// msg = fmt.Sprintf("<b><u>%s:</u></b>\n%s",
+		// 	// 	m.Sender.FirstName,
+		// 	// 	m.Text)
+		// } else if m.IsReply() {
+		// 	msg = m.Text
+		// 	rec = &telebot.User{ID: msgs[m.ReplyTo.ID]}
+		// }
+
+		rec = &telebot.User{ID: conf.OwnerId}
+		msg = fmt.Sprintf("<b><u>%s:</u></b>\n%s",
+			m.Sender.FirstName,
+			m.Text)
 
 		mn, err := bot.Send(rec, msg, telebot.NoPreview)
 		if err != nil {
@@ -30,6 +35,14 @@ func textCommand(c telebot.Context) error {
 		}
 
 		msgs[mn.ID] = m.Sender.ID
+	} else if m.IsReply() {
+		msg = m.Text
+		rec = &telebot.User{ID: msgs[m.ReplyTo.ID]}
+
+		_, err := bot.Send(rec, msg, telebot.NoPreview)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 
 	return nil
